dc7bf64de00d8d7cef6a02cb99cc6a7ba94a5560
Bouhbouh <ziad2001@live.nl>
2023-04-28 22:00:14 +0200

    functies toegevoegd

--- helper.py
+++ helper.py
@@ -1,7 +1,7 @@
-tekst="header"
-lengte = len(tekst) + 4
-print()
-print(lengte * "*")
-print(f"* {tekst} *")
-print(lengte * "*")
-print()
+def decoreer(tekst=""):
+    lengte = len(tekst) + 4
+    print()
+    print(lengte * "*")
+    print(f"* {tekst} *")
+    print(lengte * "*")
+    print()
--- tijdelijk.py
+++ tijdelijk.py
@@ -1,26 +1,33 @@
-prijzen = {
-    "aardbei" : 3 ,
-    "vanille" : 4 ,
-    "chocolade" : 5 
-}
+from helper import decoreer
 
-aanbieding_prijs = prijzen["vanille"] * 0.8
-aanbieding = "3.20"
-reclame_tekst = ("Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts €" + aanbieding)
-# 4  * 0.8 geeft 3.2 - In het voorbeeld staat dat er 2.4... moet komen, maar dan zou het om aardbei moeten gaan die de waarde 3 heeft.
+def print_aanbieding():
 
-reclame_tekst2 = (reclame_tekst[:62])
-reclame_tekst3 = (reclame_tekst2).upper()
-reclame_tekst4 = ['VANDAAG', 'IN', 'DE', 'AANBIEDING:', 'VANILLE-IJS,', '1', 'LITER', '–', 'SLECHTS', '€' + aanbieding]
+    prijzen = {
+        "aardbei" : 3 ,
+        "vanille" : 4 ,
+        "chocolade" : 5 
+    }
 
-for el in reclame_tekst4:
-    
-    if el[4:]:
-        print(el)
+    aanbieding_prijs = prijzen["vanille"] * 0.8
+    aanbieding = "3.20"
+    reclame_tekst = ("Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts €" + aanbieding)
+    # 4  * 0.8 geeft 3.2 - In het voorbeeld staat dat er 2.4... moet komen, maar dan zou het om aardbei moeten gaan die de waarde 3 heeft.
+
+    reclame_tekst2 = (reclame_tekst[:62])
+    reclame_tekst3 = (reclame_tekst2).upper()
+    reclame_tekst4 = ['VANDAAG', 'IN', 'DE', 'AANBIEDING:', 'VANILLE-IJS,', '1', 'LITER', '–', 'SLECHTS', '€' + aanbieding]
+
+    for el in reclame_tekst4:
         
-    else:
-        print(el.lower())
-      
-    
+        if el[4:]:
+            print(el)
+            
+        else:
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()
+
+
 
 
